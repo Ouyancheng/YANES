349f61ceec994723bf23058da66814d15b166753
Ouyancheng <1024842937@qq.com>
2022-06-13 16:17:33 -0700

    some code on cpu

--- src/cpu.c
+++ src/cpu.c
@@ -1,27 +1,115 @@
+/**
+ * The NES's 6502 CPU 
+ * Very useful information: 
+ * Opcode description: https://llx.com/Neil/a2/opcodes.html 
+ * The 6502 instruction set: https://www.masswerk.at/6502/6502_instruction_set.html 
+ * The illegal opcodes: https://www.masswerk.at/nowgobang/2021/6502-illegal-opcodes 
+ * The opcode table: https://www.nesdev.org/wiki/CPU_unofficial_opcodes 
+ * The 6502 simulator: http://www.visual6502.org/JSSim/expert.html 
+ * The instructions: http://www.6502.org/tutorials/6502opcodes.html 
+ */
 #include "cpu.h"
 #include "cpu_opcodes.h"
-
-void cpu_init(struct nescpu *cpu) {
-    cpu->a = 0; 
-    cpu->x = 0; 
-    cpu->y = 0; 
-    cpu->sp = CPU_SP_RESET; 
-    cpu->p = (uint8_t)cpu_flag_break2 | (uint8_t)cpu_flag_interrupt_disable; 
-    cpu->pc = 0; 
+#define internal_function static inline
+internal_function void set_flag_carry(struct nescpu *cpu, bool val) { 
+    // cpu->p = (val) ? (cpu->p | (uint8_t)cpu_flag_carry) : (cpu->p & (~(uint8_t)cpu_flag_carry)); 
+    cpu->flag_C = val; 
 }
-
-const enum addressmode type1_bbb_to_addrmode[8] = {
-    [0b000] = addressmode_indirect_x, // len=2, cyc=6
-    [0b001] = addressmode_zeropage,   // len=2, cyc=3
-    [0b010] = addressmode_immediate,  // len=2, cyc=2
-    [0b011] = addressmode_absolute,   // len=3, cyc=4
-    [0b100] = addressmode_indirect_y, // len=2, cyc=5+1 (STA: always+1, others+1 if page crossed)
-    [0b101] = addressmode_zeropage_x, // len=2, cyc=4
-    [0b110] = addressmode_absolute_y, // len=3, cyc=4+1 (STA: always+1, others+1 if page crossed)
-    [0b111] = addressmode_absolute_x, // len=3, cyc=4+1 (STA: always+1, others+1 if page crossed)
+internal_function void set_flag_zero(struct nescpu *cpu, bool val) { 
+    // cpu->p = (val) ? (cpu->p | (uint8_t)cpu_flag_zero) : (cpu->p & (~(uint8_t)cpu_flag_zero)); 
+    cpu->flag_Z = val; 
+}
+internal_function void set_flag_IRQ(struct nescpu *cpu, bool val) { 
+    // cpu->p = (val) ? (cpu->p | (uint8_t)cpu_flag_interrupt_disable) : (cpu->p & (~(uint8_t)cpu_flag_interrupt_disable)); 
+    cpu->flag_I = val; 
+}
+internal_function void set_flag_decimal(struct nescpu *cpu, bool val) { 
+    // cpu->p = (val) ? (cpu->p | (uint8_t)cpu_flag_decimal_mode) : (cpu->p & (~(uint8_t)cpu_flag_decimal_mode)); 
+    cpu->flag_D = val; 
+}
+internal_function void set_flag_overflow(struct nescpu *cpu, bool val) { 
+    // cpu->p = (val) ? (cpu->p | (uint8_t)cpu_flag_overflow) : (cpu->p & (~(uint8_t)cpu_flag_overflow)); 
+    cpu->flag_V = val; 
+}
+internal_function void set_flag_negative(struct nescpu *cpu, bool val) { 
+    // cpu->p = (val) ? (cpu->p | (uint8_t)cpu_flag_negative) : (cpu->p & (~(uint8_t)cpu_flag_negative)); 
+    cpu->flag_N = val; 
+}
+internal_function bool get_flag_carry(struct nescpu *cpu) { 
+    // return cpu->p & (uint8_t)cpu_flag_carry; 
+    return cpu->flag_C; 
+}
+internal_function bool get_flag_zero(struct nescpu *cpu) { 
+    // return cpu->p & (uint8_t)cpu_flag_zero; 
+    return cpu->flag_Z; 
+}
+internal_function bool get_flag_IRQ(struct nescpu *cpu) { 
+    // return cpu->p & (uint8_t)cpu_flag_interrupt_disable; 
+    return cpu->flag_I; 
+}
+internal_function bool get_flag_decimal(struct nescpu *cpu) { 
+    // return cpu->p & (uint8_t)cpu_flag_decimal_mode; 
+    return cpu->flag_D; 
+}
+internal_function bool get_flag_overflow(struct nescpu *cpu) { 
+    // return cpu->p & (uint8_t)cpu_flag_overflow; 
+    return cpu->flag_V; 
+}
+internal_function bool get_flag_negative(struct nescpu *cpu) { 
+    // return cpu->p & (uint8_t)cpu_flag_negative; 
+    return cpu->flag_N; 
+}
+internal_function void set_flags_from_byte(struct nescpu *cpu, uint8_t p) {
+    // cpu->p = p; 
+    set_flag_carry(cpu, p & cpu_flag_carry); 
+    set_flag_zero(cpu, p & cpu_flag_zero); 
+    set_flag_IRQ(cpu, p & cpu_flag_interrupt_disable); 
+    set_flag_decimal(cpu, p & cpu_flag_decimal_mode); 
+    set_flag_overflow(cpu, p & cpu_flag_overflow); 
+    set_flag_negative(cpu, p & cpu_flag_negative); 
+}
+internal_function uint8_t get_flags_to_byte(struct nescpu *cpu, bool flag_brk) {
+    // return cpu->p; 
+    return (
+        ((uint8_t)get_flag_carry(cpu) << 0) | 
+        ((uint8_t)get_flag_zero(cpu) << 1) | 
+        ((uint8_t)get_flag_IRQ(cpu) << 2) | 
+        ((uint8_t)get_flag_decimal(cpu) << 3) | 
+        ((uint8_t)flag_brk << 4) | 
+        (UINT8_C(1) << 5) | 
+        ((uint8_t)get_flag_overflow(cpu) << 6) | 
+        ((uint8_t)get_flag_negative(cpu) << 7)
+    ); 
+}
+struct bbb_table_entry {
+    enum addressmode mode; 
+    unsigned length; 
+    unsigned base_cycle;
+};
+const struct bbb_table_entry type1_bbb_lookup_table[8] = {
+    [0b000] = {addressmode_indirect_x, 2, 6}, // len=2, cyc=6
+    [0b001] = {addressmode_zeropage,   2, 3}, // len=2, cyc=3
+    [0b010] = {addressmode_immediate,  2, 2}, // len=2, cyc=2
+    [0b011] = {addressmode_absolute,   3, 4}, // len=3, cyc=4
+    [0b100] = {addressmode_indirect_y, 2, 5}, // len=2, cyc=5+1 (STA: always+1, others+1 if page crossed)
+    [0b101] = {addressmode_zeropage_x, 2, 4}, // len=2, cyc=4
+    [0b110] = {addressmode_absolute_y, 3, 4}, // len=3, cyc=4+1 (STA: always+1, others+1 if page crossed)
+    [0b111] = {addressmode_absolute_x, 3, 4}, // len=3, cyc=4+1 (STA: always+1, others+1 if page crossed)
+};
+const struct bbb_table_entry type2_bbb_lookup_table[8] = {
+    [0b000] = {addressmode_immediate,  2, 2}, 
+    [0b001] = {addressmode_zeropage,   2, 5},
+    [0b010] = {addressmode_accumulator,1, 2}, 
+    [0b011] = {addressmode_absolute,   3, 6}, 
+    [0b100] = {addressmode_implicit,   1, 2}, // unofficial, the STP column, halts the cpu, reset required
+    [0b101] = {addressmode_zeropage_x, 2, 6}, 
+    [0b110] = {addressmode_implicit,   1, 2}, // unofficial NOPs, except for TXS and TSX 
+    [0b111] = {addressmode_absolute_x, 3, 7}, 
 };
 
-static void execute_type1_ALU_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb) {
+internal_function void execute_type1_ALU_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb) {
+    assert(0 <= aaa && aaa < 8); 
+    assert(0 <= bbb && bbb < 8); 
     switch (aaa) {
         case 0b000: 
             // ORA
@@ -52,9 +140,11 @@ static void execute_type1_ALU_instruction(struct nescpu *cpu, const struct cpu_o
             break; 
     }
 }
-static void execute_type2_RMW_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb); 
-static void execute_type0_control_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb); 
-static void execute_type3_unofficial_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb); 
+internal_function void execute_type2_RMW_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb); 
+internal_function void execute_type0_control_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb); 
+internal_function void execute_type3_unofficial_instruction(struct nescpu *cpu, const struct cpu_opcode *opcode_entry, unsigned aaa, unsigned bbb) {
+    panic("TODO: opcode aaa=%x bbb=%x cc=11 is an unofficial opcode\n", aaa, bbb); 
+}
 
 void cpu_run(struct nescpu *cpu, void(*callback)(struct nescpu *c)) {
     while (1) {
@@ -63,12 +153,12 @@ void cpu_run(struct nescpu *cpu, void(*callback)(struct nescpu *c)) {
         }
         uint8_t opcode = cpu_read8(cpu, cpu->pc); 
         const struct cpu_opcode *opcode_entry = &(opcode_table[opcode]);  
-        cpu->pc += 1; 
+        cpu->pc += 1; // increment the PC by 1 
         // Most instructions that explicitly reference memory locations have bit patterns of the form aaabbbcc. 
         // The aaa and cc bits determine the opcode, and the bbb bits determine the addressing mode.
-        unsigned aaa = (opcode >> 5);
+        unsigned aaa = (opcode >> 5) & 0b111;
         unsigned bbb = (opcode >> 2) & 0b111; 
-        unsigned cc = (opcode & 0b11); 
+        unsigned cc  = (opcode >> 0) & 0b11; 
 
         switch (cc) {
             case 0b01: 
@@ -93,3 +183,32 @@ void cpu_run(struct nescpu *cpu, void(*callback)(struct nescpu *c)) {
         }
     }
 }
+
+/// https://www.nesdev.org/wiki/CPU_power_up_state 
+void cpu_init(struct nescpu *cpu) {
+    cpu->a = 0; 
+    cpu->x = 0; 
+    cpu->y = 0; 
+    cpu->sp = CPU_POWERUP_SP; 
+    // 0b0011_0100 or 0x34
+    // cpu->p = (uint8_t)cpu_flag_ignored | (uint8_t)cpu_flag_break | (uint8_t)cpu_flag_interrupt_disable; 
+    cpu->flag_C = 0;
+    cpu->flag_Z = 0; 
+    cpu->flag_I = 1; 
+    cpu->flag_D = 0; 
+    cpu->flag_V = 0; 
+    cpu->flag_N = 0; 
+    cpu->pc = 0; // ? 
+}
+/// https://www.nesdev.org/wiki/CPU_power_up_state 
+void cpu_reset(struct nescpu *cpu) {
+    cpu->sp -= 3; 
+    // cpu->p |= (uint8_t)cpu_flag_interrupt_disable; 
+    cpu->flag_I = 1; 
+    cpu->pc = cpu_read16(cpu, CPU_RESET_VECTOR); 
+}
+
+
+
+
+
--- src/cpu.h
+++ src/cpu.h
@@ -1,14 +1,21 @@
 #pragma once 
 
 #include "sdk.h"
-
 struct nescpu {
     uint8_t a; 
     uint8_t x; 
     uint8_t y; 
     uint8_t sp; 
-    uint8_t p; 
+    // uint8_t p; 
     uint16_t pc; 
+    /// The expanded flags of the p register 
+    bool flag_C; // bit 0: carry 
+    bool flag_Z; // bit 1: zero
+    bool flag_I; // bit 2: IRQ/i nterrupt disable
+    bool flag_D; // bit 3: decimal, not supported 
+    // there's no break flags in the P register (bit 4 and bit 5)
+    bool flag_V; // bit 6: overflow 
+    bool flag_N; // bit 7: negative 
 };
 
 enum cpu_flags {
@@ -17,11 +24,16 @@ enum cpu_flags {
     cpu_flag_interrupt_disable = (0b00000100),
     cpu_flag_decimal_mode      = (0b00001000), // not supported 
     cpu_flag_break             = (0b00010000),
-    cpu_flag_break2            = (0b00100000),
+    cpu_flag_ignored           = (0b00100000), // usually ignored 
     cpu_flag_overflow          = (0b01000000), 
     cpu_flag_negative          = (0b10000000)
 };
-#define CPU_SP_RESET UINT8_C(0xFD)
+
+
+#define CPU_POWERUP_SP UINT8_C(0xFD)
+#define CPU_IRQ_VECTOR UINT16_C(0xFFFE)
+#define CPU_RESET_VECTOR UINT16_C(0xFFFC)
+#define CPU_NMI_VECTOR UINT16_C(0xFFFA)
 
 void cpu_init(struct nescpu *cpu); 
 void cpu_copy(struct nescpu *to_cpu, const struct nescpu *cpu); 
